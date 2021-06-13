8e753ce639a81a820b19e7681a1903df316e46d7
astudilloalex <astudilloalex1@gmail.com>
2021-06-13 17:41:45 -0500

    Remove unused lines

--- lib/simple_ink.dart
+++ lib/simple_ink.dart
@@ -1,7 +1,2 @@
-library simple_ink;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/constants.dart';
+export 'src/ink.dart';
