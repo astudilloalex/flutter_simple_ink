b5b625eb60e553a16a780cc634d06e8d45ffa381
astudilloalex <astudilloalex1@gmail.com>
2021-06-13 17:38:54 -0500

    Add onTap GestureTapCallback required and HoverInkAnim

--- lib/src/ink.dart
+++ lib/src/ink.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:simple_ink/src/constants.dart';
+import 'package:simple_ink/src/enums.dart';
 import 'package:simple_ink/src/ink_well.dart';
 
+///Create an ink
 class SimpleInk extends StatelessWidget {
+  final HoverInkAnim hoverInkAnim;
   final InkType inkType;
+  final GestureTapCallback onTap;
 
-  SimpleInk({required this.inkType});
+  SimpleInk({
+    required this.hoverInkAnim,
+    required this.inkType,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SimpleInkWell();
+    if (inkType == InkType.Well) {
+      switch (hoverInkAnim.hoverAnim) {
+        case HoverAnim.UnderlineText:
+          return InkWellUnderlineText(
+            color: hoverInkAnim.color,
+            text: hoverInkAnim.text,
+            textStyle: hoverInkAnim.textStyle,
+            onTap: onTap,
+          );
+        case HoverAnim.Underline:
+          return InkWellUnderline(
+            color: hoverInkAnim.color,
+            child: hoverInkAnim.child,
+          );
+      }
+    } else
+      return InkWell();
   }
 }
