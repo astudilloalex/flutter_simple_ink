b8b079c4d9f4edfc9ac5811717607d4719764a09
astudilloalex <astudilloalex1@gmail.com>
2021-06-13 17:36:52 -0500

    Add HoverInkAnim to add animation on hover

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,3 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:simple_ink/src/enums.dart';
+
 enum InkType {
   Well,
+}
+
+class HoverInkAnim {
+  late final Widget child;
+  final Color color;
+  late final HoverAnim hoverAnim;
+  late final String text;
+  late final TextAlign? textAlign;
+  late final TextStyle? textStyle;
+
+  HoverInkAnim.underline({
+    this.color = Colors.white,
+    required this.child,
+  }) {
+    hoverAnim = HoverAnim.Underline;
+  }
+
+  HoverInkAnim.underlineText({
+    this.color = Colors.white,
+    required this.text,
+    this.textAlign,
+    this.textStyle,
+  }) {
+    hoverAnim = HoverAnim.UnderlineText;
+  }
 }
